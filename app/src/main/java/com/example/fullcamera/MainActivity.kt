412503c9a8bf94de783a7794540a511b690f7ca8
zsh1031 <zsh1031@outlook.com>
2026-06-12 21:22:05 +0800

    Add files via upload

--- app/src/main/java/com/example/fullcamera/MainActivity.kt
+++ app/src/main/java/com/example/fullcamera/MainActivity.kt
@@ -1,2 +1,9 @@
+
 package com.example.fullcamera
-class MainActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
